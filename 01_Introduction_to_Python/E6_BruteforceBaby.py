b6059752e84de4370358c28adc9fd19747c937cf
kotteru <erukotteri@gmail.com>
2025-11-07 12:57:02 +0400

    you might have dementia

---
+++ 01_Introduction_to_Python/E6_BruteforceBaby.py
@@ -0,0 +1,10 @@
+correct_password = "12345"
+
+while True:
+    user_password = input("Enter the password: ")
+    
+    if user_password == correct_passwor
+        print("Access granted! Welcome.")
+        break
+    else:
+        print("Do you have dementia?. Try again.")
