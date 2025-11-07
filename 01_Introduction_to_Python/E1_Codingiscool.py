c25e4ce7b02174c1d0038201baf258203b70183a
kotteru <erukotteri@gmail.com>
2025-11-07 11:43:20 +0400

    Add Python code to print 'Coding is Cool'

---
+++ 01_Introduction_to_Python/E1_Codingiscool.py
@@ -0,0 +1,7 @@
+# Fill in the blanks in the Python code below to output the phrase "Coding is Cool" to the console using variables and string concatenation.
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+ 
+# Use string concatenation to combine the variables and print the phrase
+print(word1 + " " + word2 + " " + word3)
