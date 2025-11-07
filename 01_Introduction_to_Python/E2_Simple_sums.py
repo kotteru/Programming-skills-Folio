562acdcf4a9d817c69105045c8c94a2f63b672b1
kotteru <erukotteri@gmail.com>
2025-11-07 11:47:36 +0400

    Simple Sums
    Updated comments for clarity and consistency.

---
+++ 01_Introduction_to_Python/E2_Simple_sums.py
@@ -0,0 +1,10 @@
+# 1. Declare a variable and initialize it with the integer value 8.
+# 2. Declare a second variable and initialize it with the integer value 10.
+# 3. Declare a third variable that stores the sum of first two numbers.
+# 4. Print the value of the sum to the console.
+num1 = 8
+num2 = 10
+
+sum_result = num1 + num2
+
+print(sum_result)
